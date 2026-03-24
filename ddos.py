030b95ef827e5b22857ed994bcaa6fff07cfaa53
avijitc035-creator <avijitc035@gmail.com>
2026-03-25 04:04:19 +0530

    Update bot token and admin chat ID in ddos.py
    Updated the bot token and admin chat ID for the DDoS script.

--- ddos.py
+++ ddos.py
@@ -8,8 +8,8 @@ import time
 from scapy.all import IP, TCP, UDP, ICMP, send, raw
 from telebot import types
 
-TOKEN = "YOUR_ACTUAL_TOKEN"
-ADMIN_CHAT_ID = YOUR_ACTUAL_ID
+TOKEN = "8230242190:AAH7S_7Rc8zBQvyKwAsNQDs8yemJ4fZLeGM"
+ADMIN_CHAT_ID = @Kailash28
 MAX_PACKETS = 1000000  # Industrial-grade flood volume
 
 bot = telebot.TeleBot(TOKEN)
